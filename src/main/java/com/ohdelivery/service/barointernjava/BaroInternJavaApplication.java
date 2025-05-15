aecd04442296a11015c279fd77dc9b712c1a6de4
sinryuji <siler2474@gmail.com>
2025-05-15 16:17:48 +0900

    init

---
+++ src/main/java/com/ohdelivery/service/barointernjava/BaroInternJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ohdelivery.service.barointernjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroInternJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaroInternJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohdelivery/service/barointernjava/BaroInternJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohdelivery.service.barointernjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaroInternJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
